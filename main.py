be266fed67095de4ef38bdf68ec698b87b00f2b4
JAAR <joe.aaron.acosta@outlook.com>
2023-07-28 16:48:12 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 07/28/2023
+>>> Sell Price Calculator
+>>> Version 0
+'''
+
+'''
+>>> Generates a program that will ask the user to input a stocks ticker, purchase price, and initial number of shares purchased in order to calculate the sell price required to retain both a specific number of shares and the entire initial investment in full.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
